8ff171119f442a8983b762d75b5cce7783efe817
Pavi <mickeynegi924@gmail.com>
2023-03-21 20:24:11 +0530

    add news navigation
    mynews

---
+++ pages/Navigation.js
@@ -0,0 +1,127 @@
+import { useState } from "react";
+import { Navbar, Nav, NavDropdown, Button } from "react-bootstrap";
+import { debounce } from "lodash";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import News from "./News";
+
+const Navigation = () => {
+  //category
+  const moreCategories = [
+    { label: "Sports", value: "sports" },
+    { label: "Google", value: "google" },
+    { label: "Sci/Fi", value: "sci" },
+    { label: "Tech", value: "technology" },
+    { label: "Health", value: "health" },
+  ];
+  //typeahead list
+  const typeahead = [
+    "technology",
+    "health",
+    "science",
+    "politics",
+    "entertainment",
+    "education",
+    "environmental",
+    "politics",
+    "sports",
+    "crime",
+    "weather",
+    "travel",
+    "lifestyle",
+    "food",
+    "arts",
+  ];
+  //date
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, "0");
+  const day = String(today.getDate() - 1).padStart(2, "0");
+  const utcDate = `${year}-${month}-${day}`;
+  //all usestates
+  const [inp, setInp] = useState("trending");
+  const [date, setDate] = useState(null);
+  const [pick, setPick] = useState(utcDate);
+  //search
+  const handleSearch = debounce((e) => {
+    setInp(e.target.value);
+  }, 1500);
+
+  return (
+    <>
+      <Navbar bg="light" expand="lg">
+        <h1 className="heading">MyNewsApp</h1>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse>
+          <Nav className="ml-auto align-items-center">
+            <h6 className="nav-link" onClick={() => setInp("business")}>
+              Business
+            </h6>
+            <h6 className="nav-link" onClick={() => setInp("entertainment")}>
+              Entertainment
+            </h6>
+            <h6 className="nav-link" onClick={() => setInp("politics")}>
+              Politics
+            </h6>
+            <NavDropdown
+              title="More Category"
+              style={{ marginBottom: "8px", fontWeight: "600" }}
+            >
+              {moreCategories.map((category) => (
+                <h6
+                  className="dropdown-item"
+                  onClick={() => setInp(category.value)}
+                  key={category.value}
+                >
+                  {category.label}
+                </h6>
+              ))}
+            </NavDropdown>
+            <div className="form-inline">
+              <div className="d-flex align-items-center">
+                <input
+                  list="typeahead"
+                  placeholder="Search Topic"
+                  className="mr-sm-2"
+                  maxResults={3}
+                  onChange={handleSearch}
+                  style={{
+                    background: "white",
+                    color: "black",
+                    borderRadius: "5px",
+                  }}
+                />
+                <datalist id="typeahead">
+                  {typeahead.map((suggestion, index) => (
+                    <option value={suggestion} key={index} />
+                  ))}
+                </datalist>
+              </div>
+            </div>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+      <div className="d-flex align-items-center col-md-3 ml-auto">
+        <DatePicker
+          selected={date}
+          onChange={(e) => setDate(e)}
+          dateFormat="yyyy-MM-dd"
+          placeholderText="Choose Date.."
+          className="form-control mr-sm-2"
+        />
+        <Button
+          variant="outline-light"
+          onClick={() => setPick(date.toISOString())}
+          disabled={!date}
+        >
+          Pick
+        </Button>
+      </div>
+
+      <News query={inp} date={pick} />
+    </>
+  );
+};
+
+export default Navigation;
---
+++ pages/News.js
@@ -0,0 +1,111 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
+
+const News = ({ query, date }) => {
+  // console.log(date);
+  const pageSize = 10; // number of items to display per page
+
+  let url = `https://newsapi.org/v2/everything?q=${query}&from=${date}&sortBy=popularity&apiKey=edf744321191485e9a67684b9d537d6b`;
+  // console.log(query);
+  const [news, setNews] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((res) => {
+        console.log(res.data.articles);
+        setNews(res.data.articles);
+      })
+      .catch((err) => {
+        // console.log(err);
+      });
+  }, [query, url, date]);
+
+  // calculate the index of the first and last items to display
+  const indexOfLastItem = currentPage * pageSize;
+  const indexOfFirstItem = indexOfLastItem - pageSize;
+  const currentItems = news.slice(indexOfFirstItem, indexOfLastItem);
+
+  // function to handle page change
+  const pagechnge = (pgno) => {
+    setCurrentPage(pgno);
+  };
+
+  return (
+    <>
+      {/* <Navigation /> */}
+
+      <div className="container">
+        <h1 className="text-capitalize mt-2" style={{ fontSize: "20px" }}>
+          :- {query}&nbsp; News
+        </h1>
+        {currentItems.map((item, key) => {
+          return (
+            <div
+              className="row bg-light text-dark mb-2 mt-3 border-dark "
+              key={key}
+            >
+              <div className="card-body col-md-8 ">
+                <h5 className="card-title">{item.title}</h5>
+
+                <p
+                  // maxLength="10"
+                  style={{
+                    display: "block",
+                    width: "81px",
+                    overflow: "hidden",
+                    whiteSpace: "nowrap",
+                    textoverflow: "ellipsis",
+                  }}
+                >
+                  {item.publishedAt}
+                </p>
+                <p className="card-text">{item.description}</p>
+                <a
+                  href={item.url}
+                  className="card-link stretched-link"
+                  target="_blank"
+                >
+                  Read More
+                </a>
+              </div>
+              <img
+                className="card-img-top col-md-4"
+                src={item.urlToImage}
+                alt="No image"
+              />
+            </div>
+          );
+        })}
+      </div>
+      <div className="d-flex justify-content-center">
+        <nav aria-label="Page navigation example">
+          <ul className="pagination">
+            {news.length > pageSize &&
+              Array(Math.ceil(news.length / pageSize))
+                .fill()
+                .map((item, index) => (
+                  <li
+                    key={index}
+                    className={`page-item ${
+                      currentPage === index + 1 ? "active" : ""
+                    }`}
+                  >
+                    <Button
+                      variant="outline-light"
+                      onClick={() => pagechnge(index + 1)}
+                    >
+                      {index + 1}
+                    </Button>
+                  </li>
+                ))}
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default News;
